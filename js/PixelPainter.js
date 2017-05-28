5ec37a3b5a38b60b1fd8d3957409a3f11bf8745b
clarkdenunzio <clark@clarkdenunzio.com>
2017-05-27 18:01:37 -1000

    new push

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,10 +1,27 @@
 var pixelProject = (function() {
+  var i = 0;
+  var j = 0;
+  var dimension = 4;
+  var ourColor = "";
 
+  var clearGrid = function(){
+    var cursor = 0;
+    var ourCursorID = "";
+    for(i = 0; i < dimension; i++){
+      for(j = 0; j < dimension; j++){
+        cursor++;
+        ourCursorID = "#".concat(cursor);
+        $(ourCursorID).css("background-color", "white");
+      }
+    }
+  };
+
+  // Build Grid Function
   var buildGrid = function() {
 
     var cursor = 0;
 
-    for (var i = 0; i < 4; i++) {
+    for (i = 0; i < dimension; i++) {
 
       var ourRowId = "row";
       ourRowId = ourRowId.concat(i);
@@ -13,24 +30,31 @@ var pixelProject = (function() {
       ourRowId = "#".concat(ourRowId);
 
 
-      for (var j = 0; j < 4; j++) {
+      for (j = 0; j < dimension; j++) {
         cursor++;
         console.log(ourRowId);
         var ourDiv = "<div class=\"pixelBox\" id=\"" + cursor + "\"> </div>";
         $(ourRowId).append(ourDiv);
         ourCursorID = "#";
         ourCursorID = ourCursorID.concat(cursor);
-        $(ourCursorID).click(insertColor());
+        $(ourCursorID).click(  function(){
+            insertColor(this.id);
+          });
       }
     }
-
-    function insertColor() {
-    
-    }
-
-
   };
 
+
+  // Insert Color
+  function insertColor(selector) {
+      // Make it JQuery-able
+      selector = "#".concat(selector);
+
+      $(selector).css("background-color", "black");
+
+  }
+
+  // Test Function
   function testFunction() {
 
     var testDiv = "<div class=\"pixelBox\">testtext</div>";
@@ -41,12 +65,17 @@ var pixelProject = (function() {
     $('#colors').append(testDiv2);
 
 
-  };
+  }
 
+
+  // Return Block
   return {
     test: testFunction,
-    build: buildGrid
+    build: buildGrid,
+    clear: clearGrid
   };
+
+
 
 });
 
